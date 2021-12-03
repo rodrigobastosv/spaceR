a2194c8584a17609dcdffd4330c6635c073a80af
Rodrigo Bastos Vasconcelos <rodrigobastosv@gmail.com>
2021-12-03 09:58:32 -0300

    adding splash animation for android

--- android/app/src/main/kotlin/com/example/space_r/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/space_r/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.space_r
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
